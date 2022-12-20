6b73c003498bb42b164ea9dd0b61840e8487b398
adam <athornton@lsst.org>
2022-12-20 11:22:27 -0700

    Syntactically OK

--- src/rsp_restspawner/spawner.py
+++ src/rsp_restspawner/spawner.py
@@ -4,39 +4,36 @@ It is designed to talk to the RSP JupyterLab Controller via a simple REST
 interface described in sqr-066.
 """
 
-import asyncio
-
-from typing import Any, Dict, Future, Optional
-
-from jupyterhub.spawner import Spawner
+from typing import Any, Dict, Optional
 
 from httpx import AsyncClient
+from jupyterhub.spawner import Spawner
 
-class RubinRSPRestSpawner(Spawner):
 
+class RSPRestSpawner(Spawner):
     def __init__(self) -> None:
         super().__init__()
         self.client = AsyncClient()
         self.pod_name = ""
-    
+
     def get_state(self) -> Dict[str, Any]:
         state = super().get_state()
         # Do something with state
-        state['pod_name'] = self.pod_name
+        state["pod_name"] = self.pod_name
         return state
 
-    def load_state(self, state) -> None:
+    def load_state(self, state: Dict[str, Any]) -> None:
         # Set our own traits from state
         pass
 
     async def start(self) -> str:
-        """Returns URL of running pod.
-        """
-    
+        """Returns URL of running pod."""
+        return ""
+
     async def stop(self) -> None:
         pass
-    
-    async def poll(self) ->Optional[int]:
+
+    async def poll(self) -> Optional[int]:
         """
         Check if the pod is running.
 
@@ -44,5 +41,3 @@ class RubinRSPRestSpawner(Spawner):
         if we know it, or 1 if it exited but we don't know how.
         """
         pass
-
-    

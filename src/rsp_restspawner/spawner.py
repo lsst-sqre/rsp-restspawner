0b1f834255ef04f6b7ea7ca099b90408bef657f9
Adam Thornton <athornton@lsst.org>
2023-03-08 11:07:23 -0700

    Make options_from_form() passthrough again; freshen deps

--- src/rsp_restspawner/spawner.py
+++ src/rsp_restspawner/spawner.py
@@ -12,7 +12,7 @@ from httpx import AsyncClient, Headers
 from jupyterhub.spawner import Spawner
 
 from .admin import get_admin_token
-from .constants import DROPDOWN_SENTINEL_VALUE, LabStatus
+from .constants import LabStatus
 from .errors import SpawnerError
 from .http import get_client
 from .util import get_external_instance_url, get_hub_base_url, get_namespace
@@ -165,21 +165,8 @@ class RSPRestSpawner(Spawner):
     def options_from_form(
         self, formdata: Dict[str, List[str]]
     ) -> Dict[str, List[str]]:
-        """We want to move the processing we are doing here back into the
-        lab controller in the form of Pydantic validators.
-
-        For some reason I do not understand, this gets called twice on the
-        way to spawning."""
-        if "size" in formdata:
-            formdata["size"] = [x.lower() for x in formdata["size"]]
-        if (
-            "image_list" in formdata
-            and formdata["image_list"][0] == DROPDOWN_SENTINEL_VALUE
-        ):
-            del formdata["image_list"]
-        else:
-            if "image_dropdown" in formdata:
-                del formdata["image_dropdown"]
+        """Do all the parsing on the controller side.  This is just a
+        passthrough."""
         return formdata
 
     async def progress(self) -> AsyncGenerator:

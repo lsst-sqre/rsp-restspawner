470e0a40ca0c1342a96eab4ec5da36096d248cbf
Russ Allbery <rra@lsst.org>
2023-03-23 16:29:15 -0700

    Clean up enums and configuration
    Stop using a mix of environment variables and files for configuration
    and instead expect configuration via traitlets like the rest of
    JupyterHub. We already have to inject mechanisms to set default values
    for those traitlets, so this lets us configure the spawner in the same
    way as everything else.
    
    Simplify the complex way of creating enums in favor of a
    straightforward str enum with defined values for the lab status.
    
    Change the default admin token path to be under /etc/gafaelfawr so
    that we can mount the whole secret on that path, which in turn will
    hopefully give us dynamic updates of the token.
    
    Delete more now-uneeded code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,7 @@
 
 from __future__ import annotations
 
-import os
 from pathlib import Path
-from typing import Generator
-from unittest import mock
 
 import pytest
 import respx
@@ -14,22 +11,6 @@ from rsp_restspawner.spawner import RSPRestSpawner
 
 from .support.controller import MockLabController, register_mock_lab_controller
 from .support.jupyterhub import MockHub, MockUser
-
-
-@pytest.fixture(autouse=True)
-def env_mock() -> Generator:
-    input_files = Path(Path(__file__).parent / "testdata")
-    with mock.patch.dict(
-        os.environ,
-        {
-            "EXTERNAL_INSTANCE_URL": "https://rsp.example.org",
-            "RESTSPAWNER_ADMIN_TOKEN_FILE": str(
-                Path(input_files / "admin-token")
-            ),
-            "RESTSPAWNER_CONFIG_FILE": str(Path(input_files / "config.yaml")),
-        },
-    ):
-        yield
 
 
 @pytest.fixture
@@ -42,10 +23,14 @@ def mock_lab_controller(respx_mock: respx.Router) -> MockLabController:
 def spawner(mock_lab_controller: MockLabController) -> RSPRestSpawner:
     """Add spawner state that normally comes from JupyterHub."""
     result = RSPRestSpawner()
+    result.admin_token_path = str(
+        Path(__file__).parent / "data" / "admin-token"
+    )
+    result.controller_url = mock_lab_controller.base_url
+    result.hub = MockHub()
     result.user = MockUser(
         name="rachel",
         auth_state={"token": "token-of-affection"},
         url="http://lab.nublado-rachel:8888",
     )
-    result.hub = MockHub()
     return result
